2ba84a07a19e59cb31c9caff05856e52b165ea27
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-282.local>
2025-12-06 11:41:48 -0800

    Working on Milestone 1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
+// COMSC210 | Lab 37 | Tanmayee Chalamalasetti
+// IDE Used: VS Code
+// Changed starter file name to main for ease
+
 #include <iostream>
+#include <string>
 using namespace std;
 
+int sum_ascii(const string& str) {
+  int sum = 0;
 
-int main() {
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
-    
-
-    return 0;
+  for (char c : str) {
+  }
 }
 
-/* 
+int main() {
+  char a = 'A';
+  cout << a << endl;
+  cout << (int)a << endl;
+  int b = 66;
+  cout << b << endl;
+  cout << (char)b << endl;
+
+  return 0;
+}
+
+/*
 These targets are present in the dataset and can be used for testing:
 536B9DFC93AF
 1DA9D64D02A0
